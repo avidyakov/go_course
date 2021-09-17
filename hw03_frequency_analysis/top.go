f350da8da3b0e0cec0a4faa8199f713afe4a4fd1
Alexander Vidyakov <avidyakov@webim-team.ru>
2021-09-17 11:50:55 +0300

    Сделал задание со звездочкой

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,7 @@
 package hw03frequencyanalysis
 
 import (
+	"errors"
 	"sort"
 	"strings"
 )
@@ -10,10 +11,25 @@ type wordFrequency struct {
 	count int
 }
 
+var emptyWord = errors.New("empty word")
+
+func processWord(rawWord string) (string, error) {
+	rawWord = strings.Trim(rawWord, ",.-")
+	rawWord = strings.ToLower(rawWord)
+
+	if len(rawWord) == 0 {
+		return "", emptyWord
+	}
+	return rawWord, nil
+}
+
 func getTextFrequency(text string) (result []wordFrequency) {
 	frequencyMap := make(map[string]int)
 	for _, word := range strings.Fields(text) {
-		frequencyMap[word] += 1
+		processedWord, err := processWord(word)
+		if err == nil {
+			frequencyMap[processedWord] += 1
+		}
 	}
 	for key, value := range frequencyMap {
 		result = append(result, wordFrequency{
@@ -24,14 +40,18 @@ func getTextFrequency(text string) (result []wordFrequency) {
 	return
 }
 
-func Top10(text string) (result []string) {
-	frequency := getTextFrequency(text)
+func sortFrequency(frequency []wordFrequency) {
 	sort.Slice(frequency, func(i, j int) bool {
 		if frequency[i].count == frequency[j].count {
 			return frequency[i].word < frequency[j].word
 		}
 		return frequency[i].count > frequency[j].count
 	})
+}
+
+func Top10(text string) (result []string) {
+	frequency := getTextFrequency(text)
+	sortFrequency(frequency)
 
 	if len(frequency) > 10 {
 		frequency = frequency[:10]
