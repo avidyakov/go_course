3a92592c2d0efed733eb12f1639d6fc5a3b096c0
Alexander Vidyakov <avidyakov@webim-team.ru>
2021-09-17 11:25:08 +0300

    Выполнил HW3

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,44 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type wordFrequency struct {
+	word  string
+	count int
+}
+
+func getTextFrequency(text string) (result []wordFrequency) {
+	frequencyMap := make(map[string]int)
+	for _, word := range strings.Fields(text) {
+		frequencyMap[word] += 1
+	}
+	for key, value := range frequencyMap {
+		result = append(result, wordFrequency{
+			word:  key,
+			count: value,
+		})
+	}
+	return
+}
+
+func Top10(text string) (result []string) {
+	frequency := getTextFrequency(text)
+	sort.Slice(frequency, func(i, j int) bool {
+		if frequency[i].count == frequency[j].count {
+			return frequency[i].word < frequency[j].word
+		}
+		return frequency[i].count > frequency[j].count
+	})
+
+	if len(frequency) > 10 {
+		frequency = frequency[:10]
+	}
+
+	for _, value := range frequency {
+		result = append(result, value.word)
+	}
+	return
 }
